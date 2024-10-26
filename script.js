2c97618c8c8a149a5c6794029783bd41c1f0940a
Rian Baltazar <rianbaltazar@gmail.com>
2024-10-25 20:16:48 -0500

    Minor Adjustments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
-    alert(`Thank you for contacting us, ${email}!`);
+    alert(`You have successfully submitted your email @, ${email}!`);
 });
